8e25ac069b7e7111f70c9ef8891b2f116dce5ceb
Russ <18russjames@gmail.com>
2020-12-24 16:51:47 -0800

    fixed api config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export default {
-    REACT_APP_API_BASE:
-        process.env.REACT_APP_API_BASE || 'https://petful-api-app-server.herokuapp.com/api',
+    REACT_APP_API_BASE: 'https://petful-api-app-server.herokuapp.com/api',
 };
