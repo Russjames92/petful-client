a15878ea31ddb33a744e63b4aa163ee4dce30fd1
Russ <18russjames@gmail.com>
2020-12-24 18:58:38 -0800

    endpoint edit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     REACT_APP_API_BASE:
-        'https://petful-api-app-server.herokuapp.com/' || 'http://localhost:8000/api',
+        'https://petful-api-app-server.herokuapp.com/api' || 'http://localhost:8000/api',
 };
